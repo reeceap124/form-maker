a5febd2afd712954f009da06ad6bec4d8665f4e9
Reece Pierson <piersonreece@gmail.com>
2021-04-14 22:16:19 -0600

    adds textbox UI

--- src/UI/index.js
+++ src/UI/index.js
@@ -1,8 +1,10 @@
 import HelloWorld from "./HelloWorld";
 import Form from "./Form";
+import TextBox from "./TextBox";
 const UI = {
   HelloWorld,
-  Form
+  Form,
+  TextBox
 };
 
 export default UI;
