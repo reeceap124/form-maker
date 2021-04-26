5c0c529092073cdb5338c34d40ba01fc186335b3
Reece Pierson <piersonreece@gmail.com>
2021-04-25 19:56:42 -0600

    starts setting up as PWA

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "semantic-ui-css/semantic.min.css";
 import UI from "./UI";
 import FormBuilder from "./Components/FormBuilder";
 const { HelloWorld, Form, TextBox } = UI;
-const texts = ["my", "test", "text", "here"];
 
 function App() {
   const MyText = HelloWorld;
@@ -11,11 +10,6 @@ function App() {
     <div className="App">
       <MyText value="Hello World" />
       <FormBuilder />
-      {/* <Form title="Form Title">
-        {texts.map((t) => {
-          return <TextBox value={t} />;
-        })}
-      </Form> */}
     </div>
   );
 }
